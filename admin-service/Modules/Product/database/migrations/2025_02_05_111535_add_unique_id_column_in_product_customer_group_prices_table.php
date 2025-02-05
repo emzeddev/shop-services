b9152854684bf9e9375161422b941e303111d01a
hassan mohammadzadeh <hsnmhmdzade@gmail.com>
2025-02-05 14:55:06 +0330

    latest commit

---
+++ admin-service/Modules/Product/database/migrations/2025_02_05_111535_add_unique_id_column_in_product_customer_group_prices_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('product_customer_group_prices', function (Blueprint $table) {
+            $table->string('unique_id')->nullable()->unique();
+        });
+
+        DB::table('product_customer_group_prices')
+            ->update(['unique_id' => DB::raw("CONCAT_WS('|', qty, product_id, customer_group_id)")]);
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('product_customer_group_prices', function (Blueprint $table) {
+            $table->dropColumn('unique_id');
+        });
+    }
+};
